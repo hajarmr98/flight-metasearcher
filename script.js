77d724e282e25488459bea589b00fb680c313292
Ulises <dr.manzanas1@gmail.com>
2020-06-17 13:21:33 +0200

    creacion de funcion pintar

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
     e.preventDefault();
-    
     let vuelo = recolectarDatos()
-
-    if(comprobarDatos(vuelo)){
-        buscadorAvion(vuelo)
-    } else {
-        alert('Hay campos vacios')
-    }
-
-})
-
-let buscadorAvion = async ({origen, destino, ida, vuelta, adultos, ninios, bebes}) => {
-    let res = await fetch(`https:/${url}/flights/from/${origen}/to/${destino}/date_1/${ida}/adults/${adultos}/date_2/${vuelta}/kids/${ninios}/babies/${bebes}`)
-    let datos = await res.json()
-    console.log(datos)
-}
-
-let comprobarDatos =  ({origen, destino, ida, vuelta, adultos}) => {
-    if(origen === '' || destino === '' || ida === '' || vuelta === '' || adultos === ''){
-    let vuelo = recolectarDatos()
-    console.log(vuelo)
 
     if (!comprobarDatos(vuelo)) {
         alert('Rellena los datos')
@@ -29,12 +9,14 @@ let comprobarDatos =  ({origen, destino, ida, vuelta, adultos}) => {
         buscadorAvion(vuelo)
         console.log("va")
     }
-    
-}
+})
+
 let buscadorAvion = async({ origen, destino, ida, vuelta, adultos, ninios, bebes }) => {
     let res = await fetch(`http://127.0.0.1:5500/flights/from/${origen}/to/${destino}/date_1/${ida}/adults/${adultos}/date_2/${vuelta}/kids/${ninios}/babies/${bebes}`)
     let datos = await res.json()
+    pintarVuelo(datos)
 }
+
 let comprobarDatos = ({ origen, destino, ida, vuelta, adultos }) => {
     if (origen === '' || destino === '' || ida === '' || vuelta === '' || adultos === '') {
         return false
@@ -42,6 +24,7 @@ let comprobarDatos = ({ origen, destino, ida, vuelta, adultos }) => {
         return true
     }
 }
+
 let recolectarDatos = () => {
     return {
         origen: document.getElementById('vuelo-origen').value,
@@ -52,4 +35,24 @@ let recolectarDatos = () => {
         ninios: document.getElementById('ninios').value,
         bebes: document.getElementById('bebes').value
     }
+}
+
+// Origen y destino son otro objeto
+
+let pintarVuelo = ({origen, destino, horarioI, horarioV,precio}) => {
+    let cajaVuelo = document.createElement('div')
+    let cajaIda = document.createElement('article')
+    let cajaVuelta = document.createElement('article')
+    let cajaDuracion = document.createElement('article')
+    let cajaPrecio = document.createElement('article')
+
+    let aeropuertoIda = document.createElement('p')
+    aeropuertoIda.innerText = origen.aeropuerto
+    let origenIda = document.createElement('p')
+    let horaIda = document.createElement('p')
+
+    let aeropuertoVuelta = document.createElement('p')
+    let origenVuelta = document.createElement('p')
+    let horaVuelta = document.createElement('p')
+
 }
