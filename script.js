104c2119d46edc3d5a3878aac9b47c0de292f137
Silvia <silope0302@gmail.com>
2020-06-17 12:51:21 +0200

    peticion con modificaciones

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
+    e.preventDefault();
+    let vuelo = recolectarDatos()
+    console.log(vuelo)
+
+    if (!comprobarDatos(vuelo)) {
+        alert('Rellena los datos')
+        console.log("coge el if")
+    } else {
+        buscadorAvion(vuelo)
+        console.log("va")
+    }
+})
+let buscadorAvion = async({ origen, destino, ida, vuelta, adultos, ninios, bebes }) => {
+    let res = await fetch(`http://127.0.0.1:5500/flights/from/${origen}/to/${destino}/date_1/${ida}/adults/${adultos}/date_2/${vuelta}/kids/${ninios}/babies/${bebes}`)
+    let datos = await res.json()
+}
+let comprobarDatos = ({ origen, destino, ida, vuelta, adultos }) => {
+    if (origen === '' || destino === '' || ida === '' || vuelta === '' || adultos === '') {
+        return false
+    } else {
+        return true
+    }
+}
+let recolectarDatos = () => {
+    return {
+        origen: document.getElementById('vuelo-origen').value,
+        destino: document.getElementById('vuelo-vuelta').value,
+        ida: document.getElementById('fechaida').value,
+        vuelta: document.getElementById('fechavuelta').value,
+        adultos: document.getElementById('adultos').value,
+        ninios: document.getElementById('ninios').value,
+        bebes: document.getElementById('bebes').value
+    }
+}
