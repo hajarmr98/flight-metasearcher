f70987eea3f572dbe2e2c99fcc18c1582a5d439b
Silvia <silope0302@gmail.com>
2020-06-26 11:45:44 +0200

    sin cambios

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const server = express()
-const request = require('request')
 const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
-const fs = require('fs')
 const port = 2424
 
 
 server.use(express.static('public'))
 
 
-server.get('/',(req,res) => res.sendfile(__dirname + '/index.html'))
+server.get('/', (req, res) => res.sendfile(__dirname + '/index.html'))
 
-server.get('/flights/from/:origen/to/:destino/date_1/:ida/adults/:adultos/date_2/:vuelta/kids/:ninios/age/:edad',(req,res) => {
+server.get('/flights/from/:origen/to/:destino/date_1/:ida/adults/:adultos/date_2/:vuelta/kids/:ninios/age/:edad', (req, res) => {
     let collectData = {
         origen: req.params.origen,
         destino: req.params.destino,
@@ -22,21 +20,20 @@ server.get('/flights/from/:origen/to/:destino/date_1/:ida/adults/:adultos/date_2
         ninios: req.params.ninios,
         edad: req.params.edad
     }
-    
-    let scrapearDatosCzech = async () => {
+
+    let scrapearDatosCzech = async() => {
         let ret = {
             datosIda: '',
             datosVuelta: ''
         }
-        try{
-            const browser = await puppeteer.launch();
+        try {
+            /*const browser = await puppeteer.launch();
             const page = await browser.newPage();
             await page.goto("https://book.csa.cz/plnext/czech_DX/Override.action?TRAVELLER_TYPE_1=ADT&TRIP_FLOW=YES&BOOKING_FLOW=REVENUE&B_LOCATION_1=BCN&E_LOCATION_1=PRG&B_DATE_1=202010220000&B_ANY_TIME_1=TRUE&B_DATE_2=202010290000&B_ANY_TIME_2=TRUE&TRIP_TYPE=R&B_LOCATION_2=PRG&E_LOCATION_2=BCN&SO_SITE_POINT_OF_SALE=MAD&SO_SITE_USER_CURRENCY_CODE=&SO_SITE_MARKET_ID=ES&PRICING_TYPE=O&EMBEDDED_TRANSACTION=FlexPricerAvailability&DISPLAY_TYPE=2&ARRANGE_BY=D&REFRESH=0&COMMERCIAL_FARE_FAMILY_1=CFFOKWEB&DATE_RANGE_VALUE_1=3&DATE_RANGE_VALUE_2=3&DATE_RANGE_QUALIFIER_1=C&DATE_RANGE_QUALIFIER_2=C&EXTERNAL_ID=172.1.2.3&SITE=P02YP02Y&LANGUAGE=ES&SO_SITE_INS_MARKET_COUNTRY=ES&SO_SITE_AIRLINE_CODE=OK&SO_SITE_IS_INSURANCE_ENABLED=TRUE#/FPOW");
-            setTimeout(async () => {
+            setTimeout(async() => {
                 let body = await page.content()
                 const $ = cheerio.load(body)
-                
-                
+
                 ret.datosIda = {
                     title: 'Vuelo de ida',
                     empresa: $('.airline-name').first().text(),
@@ -72,16 +69,62 @@ server.get('/flights/from/:origen/to/:destino/date_1/:ida/adults/:adultos/date_2
                     duration: $('.duration').children().last().text().split(',')[0]
                 }
                 browser.close()
+                res.send(JSON.stringify(ret))*/
+
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto("https://www.eurowings.com/es/reservar/vuelos/busqueda-de-vuelos.html?destination=LHR&triptype=r&origin=LPA&fromdate=2020-10-25&todate=2020-11-14&adults=1&childs=0&infants=0&lng=es-ES#/reservar-vuelos/select");
+            setTimeout(async() => {
+                let body = await page.content()
+                const $ = cheerio.load(body)
+
+                console.log($('.m-ibe-flighttable__item .m-ibe-flighttable__item-cell.m-ibe-flighttable__flight .m-ibe-flighttable__station').text().first())
+                    /*ret.datosIda = {
+                        title: 'Vuelo de ida',
+                        empresa: $('.airline-name').first().text(),
+                        origin: {
+                            aeropuertoSalida: $('.m-ibe-flighttable__station').html(),
+                            origen: ,
+                            horarioSalida: $('.a-headline.a-headline--h4.t-spacing--0').html(),
+                            fechaSalida: $('.tripsummary-title.tripsummary-date.tripsummary-details').first().children().last().text()
+                        },
+                        destiny: {
+                            aeropuertoLlegada: $('.flight-details-arrival > .flight-details-airport').first().text(),
+                            destino: $('.flight-details-arrival > .flight-details-city').first().text().split(' ')[1],
+                            horarioLlegada: $('.m-ibe-flighttable__item-cell.m-ibe-flighttable__flight.m-ibe-flighttable__flight--right .a-headline.a-headline--h4.t-spacing--0').html().split('<')[0]
+                        },
+                        price: $('.availability-group-cell-price > .cell-reco-currency').first().text() + ' ' + $('.cell-reco-bestprice-integer.availability-font-xlarge').first().text(),
+                        duration: $('.duration').children().text().split(',')[0]
+                    }
+                    console.log(datosIda)
+                    ret.datosVuelta = {
+                        title: 'Vuelo de Vuelta',
+                        empresa: $('.airline-name').first().text(),
+                        origin: {
+                            aeropuertoSalida: $('.flight-details-departure .flight-details-airport').last().text(),
+                            origen: $('.flight-details-departure .flight-details-city').last().html().split('</span> ')[1].split('\n')[0],
+                            horarioSalida: $('.flight-details-departure .flight-details-city').last().html().split('</span> ')[0].split('\n')[2],
+                            fechaSalida: $('.tripsummary-title.tripsummary-date.tripsummary-details').last().children().last().text()
+                        },
+                        destiny: {
+                            aeropuertoLlegada: $('.flight-details-arrival > .flight-details-airport').last().text(),
+                            destino: $('.flight-details-arrival > .flight-details-city').last().text().split(' ')[1],
+                            horarioLlegada: $('.flight-details-arrival > .flight-details-city').last().text().split(' ')[0]
+                        },
+                        price: $('.availability-group-cell-price > .cell-reco-currency').first().text() + ' ' + $('.cell-reco-bestprice-integer.availability-font-xlarge').text().substring(20, 25),
+                        duration: $('.duration').children().last().text().split(',')[0]
+                    }*/
+                browser.close();
                 res.send(JSON.stringify(ret))
-            },5000)
-        }catch(err){
+            }, 5000)
+        } catch (err) {
             throw err
-        } 
-        
+        }
+
     }
 
     scrapearDatosCzech()
-    
+
 })
 
-server.listen(port,() => console.log('Running in port: ' + port))
+server.listen(port, () => console.log('Running in port: ' + port))
