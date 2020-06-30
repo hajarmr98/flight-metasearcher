c40ca1f083293b1ae4c191b39b0e734554e3aca7
Ulises <dr.manzanas1@gmail.com>
2020-06-30 17:08:35 +0200

    heroku despliegue

--- public/script.js
+++ public/script.js
@@ -168,7 +168,7 @@ let pintarAlert = () => {
 }
 
 let buscadorAvion = async({ origen, destino, ida, vuelta, adultos, ninios, bebes }) => {
-    let res = await fetch(`http://localhost:2424/flights/from/${origen}/to/${destino}/date_1/${ida}/adults/${adultos}/date_2/${vuelta}/kids/${ninios}/age/${bebes}`)
+    let res = await fetch(`/flights/from/${origen}/to/${destino}/date_1/${ida}/adults/${adultos}/date_2/${vuelta}/kids/${ninios}/age/${bebes}`)
     let datos = await res.json()
     borrarVuelos();
     if(datos.val){
