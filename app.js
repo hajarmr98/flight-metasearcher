c4fc408081d756961cb7ef9e24e07f64912faf7d
Hajar Mrabet <hajarmr98@gmail.com>
2020-09-08 21:53:41 +0200

    adding files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const server = express();
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
-const axios = require('axios');
+const express = require('express')
+const server = express()
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+const axios = require('axios')
 const port = 2424;
+
+
 
 server.use(express.static('public'))
 
@@ -83,7 +85,7 @@ let urlBebesCzech = (num) => {
     return res
 }
 
-let scrapearDatosEuroWings = async ({ida,vuelta},{origen: ori, destino: desti,ida: fechaIda,adultos,vuelta: fechaVuelta,ninios,bebes}) => {
+let scrapearDatosEuroWings = async ({ida,vuelta},{ida: fechaIda,adultos,vuelta: fechaVuelta,ninios,bebes}) => {
     let ret = {
         valid: '',
         datosIda: '',
@@ -98,9 +100,7 @@ let scrapearDatosEuroWings = async ({ida,vuelta},{origen: ori, destino: desti,id
         for(let x = 0; x < ida.length; x++){
             for(let y = 0; y < vuelta.length; y++){
                 let url = `https://www.eurowings.com/es/reservar/vuelos/busqueda-de-vuelos.html?destination=${vuelta[y]}&triptype=r&origin=${ida[x]}&fromdate=${fechaIda}&todate=${fechaVuelta}&adults=${adultos}&childs=${ninios}&infants=${bebes}&lng=es-ES#/reservar-vuelos/select`
-                const browser = await puppeteer.launch({
-                    args: ['--no-sandbox', '--disable-setuid-sandbox']
-                });
+                const browser = await puppeteer.launch();
                 const page = await browser.newPage();
                 await page.goto(url);
                 setWings = new Promise((resolve,rej) => {
@@ -115,13 +115,13 @@ let scrapearDatosEuroWings = async ({ida,vuelta},{origen: ori, destino: desti,id
                                     empresa: 'Eurowings',
                                     origin: {
                                         aeropuertoSalida: $('#flightselection__outbound .m-ibe-flighttable__station').first().text(), 
-                                        origen: ori,
+                                        origen: $('.m-form-autocomplete__prefix').first().text(),
                                         horarioSalida: $('.a-headline.a-headline--h4.t-spacing--0').html(),
                                         fechaSalida: $('.o-ibe-flightselection__navigation-action-date').text().substring(10, 20)
                                     },
                                     destiny: {
                                             aeropuertoLlegada: $('#flightselection__outbound .m-ibe-flighttable__station').last().text(),
-                                            destino: desti,
+                                            destino: $('.a-headline.a-headline--h4.t-spacing--5').first().text().split('- ')[1],
                                             horarioLlegada: $('#flightselection__outbound .a-headline.a-headline--h4.t-spacing--0').text().substring(5,10)
                                         },
                                         price: $('.a-price.a-price--large').first().text(),
@@ -133,13 +133,13 @@ let scrapearDatosEuroWings = async ({ida,vuelta},{origen: ori, destino: desti,id
                                             empresa: 'Eurowings',
                                             origin: {
                                                     aeropuertoSalida: $('#flightselection__inbound .m-ibe-flighttable__station').first().text(),
-                                                    origen: desti ,
+                                                    origen: $('.a-headline.a-headline--h4.t-spacing--5').first().text().split('- ')[1] ,
                                                     horarioSalida: $('#flightselection__inbound .m-ibe-flighttable__item-cell.m-ibe-flighttable__flight .a-headline.a-headline--h4.t-spacing--0').first().text(),
                                                     fechaSalida: $('#flightselection__inbound .o-ibe-flightselection__navigation-action-date').text().substring(10, 19),
                                     },
                                     destiny: {
                                             aeropuertoLlegada: $('#flightselection__inbound .m-ibe-flighttable__station').last().text(),
-                                            destino: ori,
+                                            destino: $('.m-form-autocomplete__prefix').first().text(),
                                             horarioLlegada: $('#flightselection__inbound .m-ibe-flighttable__item-cell.m-ibe-flighttable__flight.m-ibe-flighttable__flight--right .a-headline.a-headline--h4.t-spacing--0').last().text(),
                                     },
                                     price: $('#flightselection__inbound .a-price.a-price--large').first().text(),
@@ -168,7 +168,7 @@ let scrapearDatosEuroWings = async ({ida,vuelta},{origen: ori, destino: desti,id
     }
 }
 
-let scrapearDatosCzech = async ({ida,vuelta},{origen: ori, destino: desti, ida: fechaIda,adultos,vuelta: fechaVuelta,ninios,bebes}) => {
+let scrapearDatosCzech = async ({ida,vuelta},{ida: fechaIda,adultos,vuelta: fechaVuelta,ninios,bebes}) => {
     let ret = {
         valid: '',
         datosIda: '',
@@ -192,9 +192,7 @@ let scrapearDatosCzech = async ({ida,vuelta},{origen: ori, destino: desti, ida: 
         for(x = 0; x < ida.length; x++){
             for(y = 0; y < vuelta.length; y++){
                 let url = `https://book.csa.cz/plnext/czech_DX/Override.action?${adults}${nini}${bebis}TRIP_FLOW=YES&BOOKING_FLOW=REVENUE&B_LOCATION_1=${ida[x]}&E_LOCATION_1=${vuelta[y]}&B_DATE_1=${fechaIda}0000&B_ANY_TIME_1=TRUE&B_DATE_2=${fechaVuelta}0000&B_ANY_TIME_2=TRUE&TRIP_TYPE=R&B_LOCATION_2=MAD&E_LOCATION_2=BCN&SO_SITE_POINT_OF_SALE=MAD&SO_SITE_USER_CURRENCY_CODE=&SO_SITE_MARKET_ID=ES&PRICING_TYPE=O&EMBEDDED_TRANSACTION=FlexPricerAvailability&DISPLAY_TYPE=2&ARRANGE_BY=D&REFRESH=0&COMMERCIAL_FARE_FAMILY_1=CFFOKWEB&DATE_RANGE_VALUE_1=3&DATE_RANGE_VALUE_2=3&DATE_RANGE_QUALIFIER_1=C&DATE_RANGE_QUALIFIER_2=C&EXTERNAL_ID=172.1.2.3&SITE=P02YP02Y&LANGUAGE=ES&SO_SITE_INS_MARKET_COUNTRY=ES&SO_SITE_AIRLINE_CODE=OK&SO_SITE_IS_INSURANCE_ENABLED=TRUE#/FPOW`
-                const browser = await puppeteer.launch({
-                    args: ['--no-sandbox', '--disable-setuid-sandbox']
-                });
+                const browser = await puppeteer.launch();
                 const page = await browser.newPage();
                 await page.goto(url);
                 setCzech = new Promise((resolve,reject) => {
@@ -207,13 +205,13 @@ let scrapearDatosCzech = async ({ida,vuelta},{origen: ori, destino: desti, ida: 
                                     empresa: 'Czech Airlines',
                                     origin: {
                                         aeropuertoSalida: $('.flight-details-airport').first().text(),
-                                        origen: ori,
+                                        origen: $('.flight-details-city').first().html().split("</span> ")[1].split('\n')[0],
                                         horarioSalida: $('.flight-details-city').first().children().text().split('\n')[1],
                                         fechaSalida: $('.tripsummary-title.tripsummary-date.tripsummary-details').first().children().last().text()
                                     },
                                     destiny: {
                                         aeropuertoLlegada: $('.flight-details-arrival > .flight-details-airport').first().text(),
-                                        destino: desti,
+                                        destino: $('.flight-details-arrival > .flight-details-city').first().text().split(' ')[1],
                                         horarioLlegada: $('.flight-details-arrival > .flight-details-city').first().text().split(' ')[0]
                                     },
                                     price: $('.availability-group-cell-price > .cell-reco-currency').first().text() + ' ' + $('#tpl4_upsell-calendar-bound0_cell-price-selected-bound-0 .price').first().text(),
@@ -225,13 +223,13 @@ let scrapearDatosCzech = async ({ida,vuelta},{origen: ori, destino: desti, ida: 
                                     empresa: 'Czech Airlines',
                                     origin: {
                                         aeropuertoSalida: $('.flight-details-departure .flight-details-airport').last().text(),
-                                        origen: desti,
+                                        origen: $('.flight-details-arrival > .flight-details-city').first().text().split(' ')[1],
                                         horarioSalida: $('.flight-details-departure .flight-details-city').last().html().split('</span> ')[0].split('\n')[2],
                                         fechaSalida: $('.tripsummary-title.tripsummary-date.tripsummary-details').last().children().last().text()
                                     },
                                     destiny: {
                                         aeropuertoLlegada: $('.flight-details-arrival > .flight-details-airport').last().text(),
-                                        destino: ori,
+                                        destino: $('.flight-details-arrival > .flight-details-city').last().text().split(' ')[1],
                                         horarioLlegada: $('.flight-details-arrival > .flight-details-city').last().text().split(' ')[0]
                                     },
                                     price: $('.availability-group-cell-price > .cell-reco-currency').first().text() + ' ' + $('#tpl4_upsell-calendar-bound1_cell-price-selected-bound-1 .price').last().text(),
@@ -331,8 +329,7 @@ let buscarVuelos = async (res,datos) => {
         val: '',
         objetoIda : '',
         objetoVuelta : '',
-        url: [],
-        err: ''
+        url: []
     }
     try{
         let euroW = await scrapearDatosEuroWings(res,datos)
@@ -364,12 +361,11 @@ let buscarVuelos = async (res,datos) => {
         } else{
             final.url.push(final.objetoIda.url, final.objetoVuelta.url)
         }
+        return final
 
     } catch(err){
-        final.err = err;
-    } finally {
-        return final
-    }
+        console.log(err)
+    } 
 }
 
 
